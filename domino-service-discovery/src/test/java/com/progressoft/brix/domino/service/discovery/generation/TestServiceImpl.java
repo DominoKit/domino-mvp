ac83143426e2a3c8132181ce9a7a4f5715b59116
rjbstr <rafat.albarouki@gmail.com>
2017-06-13 13:26:42 +0300

    adding service discovery module to be part of the domino itself, also adding the service discovery instance to vertx context

---
+++ domino-service-discovery/src/test/java/com/progressoft/brix/domino/service/discovery/generation/TestServiceImpl.java
@@ -0,0 +1,4 @@
+package com.progressoft.brix.domino.service.discovery.generation;
+
+public class TestServiceImpl implements TestService {
+}
---
+++ domino-service-discovery/src/test/java/com/progressoft/brix/domino/service/discovery/generation/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(name = "test", groupPackage = "com.progressoft.brix.domino.service.discovery.generation")
+package com.progressoft.brix.domino.service.discovery.generation;
+
+import io.vertx.codegen.annotations.ModuleGen;
