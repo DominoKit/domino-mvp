280971c37cca23cf26345a13e692e5a1751314b0
Ahmad Bawaneh <akabme@gmail.com>
2017-08-20 09:41:37 +0300

    add static import

--- domino-gwt/src/main/java/com/progressoft/brix/domino/gwt/client/history/DominoDirectState.java
+++ domino-gwt/src/main/java/com/progressoft/brix/domino/gwt/client/history/DominoDirectState.java
@@ -1,20 +1,21 @@
 package com.progressoft.brix.domino.gwt.client.history;
 
-import com.progressoft.brix.domino.api.shared.history.DominoHistory;
 import com.progressoft.brix.domino.api.shared.history.TokenFilter;
 
-public class DominoDirectState implements DominoHistory.DirectState {
+import static com.progressoft.brix.domino.api.shared.history.DominoHistory.*;
+
+public class DominoDirectState implements DirectState {
 
     private final TokenFilter tokenFilter;
-    private final DominoHistory.State state;
+    private final State state;
 
-    public DominoDirectState(TokenFilter tokenFilter, DominoHistory.State state) {
+    public DominoDirectState(TokenFilter tokenFilter, State state) {
         this.tokenFilter = tokenFilter;
         this.state = state;
     }
 
     @Override
-    public void onDirectUrl(DominoHistory.DirectUrlHandler handler) {
+    public void onDirectUrl(DirectUrlHandler handler) {
         if (tokenFilter.filter(state.token().value()))
             handler.handle(state);
     }
