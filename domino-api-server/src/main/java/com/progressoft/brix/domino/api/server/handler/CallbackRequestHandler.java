06ea64337bad68ff0310b901c6817215f67a4302
Ahmad Bawaneh <akabme@gmail.com>
2017-08-22 13:01:21 +0300

    add redirectable interface for CallBackRespopnse

--- domino-api-server/src/main/java/com/progressoft/brix/domino/api/server/handler/CallbackRequestHandler.java
+++ domino-api-server/src/main/java/com/progressoft/brix/domino/api/server/handler/CallbackRequestHandler.java
@@ -1,5 +1,6 @@
 package com.progressoft.brix.domino.api.server.handler;
 
+import com.progressoft.brix.domino.api.shared.request.RedirectableResponse;
 import com.progressoft.brix.domino.api.shared.request.ServerRequest;
 import com.progressoft.brix.domino.api.shared.request.ServerResponse;
 
@@ -7,8 +8,7 @@ import com.progressoft.brix.domino.api.shared.request.ServerResponse;
 public interface CallbackRequestHandler<R extends ServerRequest, S extends ServerResponse> {
     void handleRequest(R request, ResponseCallback<S> responseCallback);
 
-    interface ResponseCallback<S> {
+    interface ResponseCallback<S> extends RedirectableResponse {
         void complete(S response);
-        void redirect(String location);
     }
 }
---
+++ domino-api-shared/src/main/java/com/progressoft/brix/domino/api/shared/request/RedirectableResponse.java
@@ -0,0 +1,5 @@
+package com.progressoft.brix.domino.api.shared.request;
+
+public interface RedirectableResponse {
+    void redirect(String location);
+}
