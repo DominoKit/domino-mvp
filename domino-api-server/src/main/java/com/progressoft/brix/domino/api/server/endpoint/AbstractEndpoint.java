cd5e678c86cc8b3351611845c25aac854a415580
Ahmad Bawaneh <akabme@gmail.com>
2017-10-22 00:24:35 +0300

    Refactor endpoints apt processor, https://github.com/GwtDomino/domino/issues/12

--- domino-api-server/src/main/java/com/progressoft/brix/domino/api/server/endpoint/AbstractEndpoint.java
+++ domino-api-server/src/main/java/com/progressoft/brix/domino/api/server/endpoint/AbstractEndpoint.java
@@ -1,7 +1,6 @@
 package com.progressoft.brix.domino.api.server.endpoint;
 
 import com.progressoft.brix.domino.api.server.ServerApp;
-import com.progressoft.brix.domino.api.server.entrypoint.VertxEntryPointContext;
 import com.progressoft.brix.domino.api.shared.request.ServerRequest;
 import com.progressoft.brix.domino.api.shared.request.ServerResponse;
 import io.vertx.core.Handler;
@@ -15,23 +14,30 @@ public abstract class AbstractEndpoint<R extends ServerRequest, S extends Server
     @Override
     public void handle(RoutingContext routingContext) {
         try {
-            ServerApp serverApp = ServerApp.make();
-            R requestBody;
-            String requestKey=routingContext.request().getHeader("REQUEST_KEY");
-            HttpMethod method = routingContext.request().method();
-            if(HttpMethod.POST.equals(method) || HttpMethod.PUT.equals(method) || HttpMethod.PATCH.equals(method)) {
-                requestBody= Json.decodeValue(routingContext.getBodyAsString(), getRequestClass());
-            } else {
-                requestBody = makeNewRequest();
-            }
-            if(nonNull(requestKey) && !requestKey.isEmpty()) {
-                requestBody.setRequestKey(requestKey);
-            } else {
-                requestBody.setRequestKey(requestBody.getClass().getCanonicalName());
-            }
-            executeRequest(routingContext, serverApp, requestBody);
+            R requestBody = fetchBody(routingContext, routingContext.request().method());
+            processRequestKey(routingContext, requestBody);
+            executeRequest(routingContext, ServerApp.make(), requestBody);
         } catch(Exception e) {
             routingContext.fail(e);
+        }
+    }
+
+    private R fetchBody(RoutingContext routingContext, HttpMethod method) {
+        R requestBody;
+        if(HttpMethod.POST.equals(method) || HttpMethod.PUT.equals(method) || HttpMethod.PATCH.equals(method)) {
+            requestBody= Json.decodeValue(routingContext.getBodyAsString(), getRequestClass());
+        } else {
+            requestBody = makeNewRequest();
+        }
+        return requestBody;
+    }
+
+    private void processRequestKey(RoutingContext routingContext, R requestBody) {
+        String requestKey=routingContext.request().getHeader("REQUEST_KEY");
+        if(nonNull(requestKey) && !requestKey.isEmpty()) {
+            requestBody.setRequestKey(requestKey);
+        } else {
+            requestBody.setRequestKey(requestBody.getClass().getCanonicalName());
         }
     }
 
