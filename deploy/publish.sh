0002864fae11983604bdf7c39ba6b956bed2b74c
Ahmad Bawaneh <akabme@gmail.com>
2018-07-24 12:26:32 +0300

    debugging travis build

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $GPG_DIR
+echo "-----------------------------------"
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     mvn clean deploy -X -Dmaven.test.skip=true --settings $GPG_DIR/settings.xml -Dci=true
     exit $?
