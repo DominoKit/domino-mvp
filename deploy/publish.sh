380c5014372de0fd33187c50c92cf5ed6d37fe1b
Ahmad Bawaneh <akabme@gmail.com>
2018-07-24 11:45:48 +0300

    add -X flag for debuging

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn clean deploy -Dmaven.test.skip=true --settings $GPG_DIR/settings.xml -Dci=true
+    mvn clean deploy -X -Dmaven.test.skip=true --settings $GPG_DIR/settings.xml -Dci=true
     exit $?
 fi
