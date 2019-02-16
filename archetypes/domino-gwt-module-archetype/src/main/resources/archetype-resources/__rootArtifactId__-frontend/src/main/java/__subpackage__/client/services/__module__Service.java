748bda75e47d3799377f54c5925bfffbf1cc23fb
Ahmad Bawaneh <akabme@gmail.com>
2019-02-16 23:36:37 +0200

    enhance request factory, use jax-rs

--- archetypes/domino-gwt-module-archetype/src/main/resources/archetype-resources/__rootArtifactId__-frontend/src/main/java/__subpackage__/client/services/__module__Service.java
+++ archetypes/domino-gwt-module-archetype/src/main/resources/archetype-resources/__rootArtifactId__-frontend/src/main/java/__subpackage__/client/services/__module__Service.java
@@ -3,11 +3,12 @@
 #set( $symbol_escape = '\' )
 package ${package}.${subpackage}.client.services;
 
-import org.dominokit.domino.api.client.annotations.Path;
 import org.dominokit.domino.api.client.annotations.RequestFactory;
 import org.dominokit.domino.api.client.request.Response;
 import ${package}.${subpackage}.shared.response.${module}Response;
 import ${package}.${subpackage}.shared.request.${module}Request;
+
+import javax.ws.rs.Path;
 
 @RequestFactory
 public interface ${module}Service {
