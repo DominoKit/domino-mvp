9ae15e9be73b7be01cd16a6096eacd9b0239748d
rjeeb <rafat.albarouki@gmail.com>
2019-03-12 17:59:12 +0200

    cleanup

--- domino-api-server/src/main/java/org/dominokit/domino/api/server/plugins/DominoAuthProvider.java
+++
@@ -1,8 +0,0 @@
-package org.dominokit.domino.api.server.plugins;
-
-import io.vertx.reactivex.ext.auth.AuthProvider;
-import org.dominokit.domino.api.server.PluginContext;
-
-public interface DominoAuthProvider {
-    AuthProvider getAuthProvider(PluginContext pluginContext);
-}
