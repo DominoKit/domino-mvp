15e0b231b484b92a8bbafbc851b570b46381e3e8
vegegoku <akabme@gmail.com>
2019-04-24 16:13:06 +0300

    move service generation to shared modules

--- archetypes/domino-gwt-module-archetype/src/main/resources/archetype-resources/__rootArtifactId__-shared/src/main/java/__subpackage__/shared/services/__module__Service.java
+++ archetypes/domino-gwt-module-archetype/src/main/resources/archetype-resources/__rootArtifactId__-shared/src/main/java/__subpackage__/shared/services/__module__Service.java
@@ -3,9 +3,9 @@
 #set( $symbol_escape = '\' )
 package ${package}.${subpackage}.shared.services;
 
-import org.dominokit.domino.api.shared.request.annotations.service.RequestFactory;
 import org.dominokit.domino.api.shared.request.Response;
-import {package}.${subpackage}.shared.response.${module}Response;
+import org.dominokit.domino.api.shared.request.service.annotations.RequestFactory;
+import ${package}.${subpackage}.shared.response.${module}Response;
 import ${package}.${subpackage}.shared.request.${module}Request;
 
 import javax.ws.rs.Path;
