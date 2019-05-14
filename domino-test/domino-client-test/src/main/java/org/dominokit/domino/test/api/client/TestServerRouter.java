aea8569252b191e323207ea190a8dd56a79ec7f2
vegegoku <akabme@gmail.com>
2019-05-14 10:58:32 +0300

    replace success and failed requests handlers for test with completedHandler

--- domino-test/domino-client-test/src/main/java/org/dominokit/domino/test/api/client/TestServerRouter.java
+++ domino-test/domino-client-test/src/main/java/org/dominokit/domino/test/api/client/TestServerRouter.java
@@ -15,13 +15,14 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.dominokit.domino.api.shared.request.Request.*;
+
 public class TestServerRouter implements RequestRouter<ServerRequest> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TestServerRouter.class);
 
     private Map<String, ResponseReply> fakeResponses = new HashMap<>();
-    private Map<String, Future<Void>> requestSuccessCompleteHandlers = new HashMap<>();
-    private Map<String, Future<Void>> requestFailCompleteHandlers = new HashMap<>();
+    private Map<String, Future<ResponseReply>> requestCompleteHandlers = new HashMap<>();
     private final RequestAsyncSender requestAsyncRunner;
     private TestRoutingListener defaultListener = new TestRoutingListener();
     private RoutingListener listener = defaultListener;
@@ -66,13 +67,12 @@ public class TestServerRouter implements RequestRouter<ServerRequest> {
         } catch (ResourcesRepository.RequestHandlerNotFound ex) {
             LOGGER.error("Request resource not found for request [" + request.getClass().getSimpleName() + "]! either fake the request or start an actual server");
             eventFactory.makeFailed(request, new FailedResponseBean(ex)).fire();
+        } catch (FakeRequestFailure ex) {
+            LOGGER.error("could not execute request : ", ex);
+            eventFactory.makeFailed(request, ex.failedResponseBean).fire();
         } catch (Exception ex) {
             LOGGER.error("could not execute request : ", ex);
-            if (ex instanceof FakeRequestFailure) {
-                eventFactory.makeFailed(request, ((FakeRequestFailure) ex).failedResponseBean).fire();
-            } else {
-                eventFactory.makeFailed(request, new FailedResponseBean(ex)).fire();
-            }
+            eventFactory.makeFailed(request, new FailedResponseBean(ex)).fire();
         }
     }
 
@@ -92,12 +92,8 @@ public class TestServerRouter implements RequestRouter<ServerRequest> {
         return defaultListener;
     }
 
-    public void addRequestSuccessCompleteHandler(Class<? extends ServerRequest> request, Future<Void> completeHandler) {
-        requestSuccessCompleteHandlers.put(getRequestKey(request), completeHandler);
-    }
-
-    public void addRequestFailCompleteHandler(Class<? extends ServerRequest> request, Future<Void> completeHandler) {
-        requestFailCompleteHandlers.put(getRequestKey(request), completeHandler);
+    public void onRequestCompleted(Class<? extends ServerRequest> request, Future<ResponseReply> completeHandler) {
+        requestCompleteHandlers.put(getRequestKey(request), completeHandler);
     }
 
     public class TestServerSuccessEvent<T> implements Event {
@@ -117,30 +113,21 @@ public class TestServerRouter implements RequestRouter<ServerRequest> {
 
         @Override
         public void process() {
-            request.applyState(new Request.ServerResponseReceivedStateContext(makeSuccessContext()));
-            completeSuccessRequest(request);
+            request.applyState(new ServerResponseReceivedStateContext(makeSuccessContext()));
+            completeSuccessRequest(request, new SuccessReply(responseBean));
         }
 
-        private Request.ServerSuccessRequestStateContext makeSuccessContext() {
-            return new Request.ServerSuccessRequestStateContext(responseBean);
-        }
-    }
-
-    private void completeSuccessRequest(ServerRequest request) {
-        String requestKey = getRequestKey(request);
-        if (requestSuccessCompleteHandlers.containsKey(requestKey)) {
-            requestSuccessCompleteHandlers.get(requestKey)
-                    .complete();
-            requestSuccessCompleteHandlers.remove(requestKey);
+        private ServerSuccessRequestStateContext makeSuccessContext() {
+            return new ServerSuccessRequestStateContext(responseBean);
         }
     }
 
-    private void completeFailRequest(ServerRequest request) {
+    private void completeSuccessRequest(ServerRequest request, ResponseReply reply) {
         String requestKey = getRequestKey(request);
-        if (requestFailCompleteHandlers.containsKey(requestKey)) {
-            requestFailCompleteHandlers.get(requestKey)
-                    .complete();
-            requestFailCompleteHandlers.remove(requestKey);
+        if (requestCompleteHandlers.containsKey(requestKey)) {
+            requestCompleteHandlers.get(requestKey)
+                    .complete(reply);
+            requestCompleteHandlers.remove(requestKey);
         }
     }
 
@@ -175,13 +162,22 @@ public class TestServerRouter implements RequestRouter<ServerRequest> {
 
         @Override
         public void process() {
-            request.applyState(new Request.ServerResponseReceivedStateContext(makeFailedContext()));
-            completeFailRequest(request);
+            request.applyState(new ServerResponseReceivedStateContext(makeFailedContext()));
+            completeFailRequest(request, new FailedReply(failedResponseBean));
 
         }
 
-        private Request.ServerFailedRequestStateContext makeFailedContext() {
-            return new Request.ServerFailedRequestStateContext(failedResponseBean);
+        private ServerFailedRequestStateContext makeFailedContext() {
+            return new ServerFailedRequestStateContext(failedResponseBean);
+        }
+    }
+
+    private void completeFailRequest(ServerRequest request, FailedReply reply) {
+        String requestKey = getRequestKey(request);
+        if (requestCompleteHandlers.containsKey(requestKey)) {
+            requestCompleteHandlers.get(requestKey)
+                    .fail(new TestFailedRequestException(reply));
+            requestCompleteHandlers.remove(requestKey);
         }
     }
 
@@ -205,18 +201,18 @@ public class TestServerRouter implements RequestRouter<ServerRequest> {
     }
 
     public interface ResponseReply {
-        ResponseBean reply() throws Exception;
+        <T> T reply();
     }
 
-    public static class SuccessReply implements ResponseReply {
-        private final ResponseBean response;
+    public static class SuccessReply<T> implements ResponseReply {
+        private final T response;
 
-        public SuccessReply(ResponseBean response) {
+        public SuccessReply(T response) {
             this.response = response;
         }
 
         @Override
-        public ResponseBean reply() {
+        public T reply() {
             return response;
         }
     }
@@ -229,16 +225,29 @@ public class TestServerRouter implements RequestRouter<ServerRequest> {
         }
 
         @Override
-        public ResponseBean reply() throws Exception {
+        public ResponseBean reply() {
             throw new FakeRequestFailure(failedResponseBean);
         }
     }
 
-    private static class FakeRequestFailure extends Exception {
+    private static class FakeRequestFailure extends RuntimeException {
         private final FailedResponseBean failedResponseBean;
 
         public FakeRequestFailure(FailedResponseBean failedResponseBean) {
             this.failedResponseBean = failedResponseBean;
         }
     }
+
+    public static final class TestFailedRequestException extends Exception {
+        private final FailedReply failedReply;
+
+        public TestFailedRequestException(FailedReply failedReply) {
+            this.failedReply = failedReply;
+        }
+
+        public FailedReply getFailedReply() {
+            return failedReply;
+        }
+    }
+
 }
