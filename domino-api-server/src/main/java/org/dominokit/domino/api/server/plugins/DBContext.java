1e9cd2cd0517c29d95a80a5bca5d148420f0d996
rjeeb <rafat.albarouki@gmail.com>
2019-06-29 02:12:00 +0300

    modify db client configurator plugin to handle h2 script files

--- domino-api-server/src/main/java/org/dominokit/domino/api/server/plugins/DBContext.java
+++ domino-api-server/src/main/java/org/dominokit/domino/api/server/plugins/DBContext.java
@@ -1,11 +1,15 @@
 package org.dominokit.domino.api.server.plugins;
 
-import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.reactivex.ext.jdbc.JDBCClient;
 
 public class DBContext {
     static JDBCClient dbClient;
 
     public static JDBCClient getDbClient() {
         return dbClient;
+    }
+
+    public static void setDbClient(JDBCClient dbClient) {
+        DBContext.dbClient = dbClient;
     }
 }
