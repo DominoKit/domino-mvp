37eff90b1bbdc0ff178b2d6c4815405c4e0c6a7f
Rafat J. Al-Barouki <rafat.albarouki@gmail.com>
2019-06-30 09:27:02 +0300

    modify db client configurator plugin to handle h2 script files (#40)

--- domino-api-server/src/main/java/org/dominokit/domino/api/server/plugins/TestDbClientConfiguratorPlugin.java
+++ domino-api-server/src/main/java/org/dominokit/domino/api/server/plugins/TestDbClientConfiguratorPlugin.java
@@ -1,22 +1,53 @@
 package org.dominokit.domino.api.server.plugins;
 
 import com.google.auto.service.AutoService;
+import io.reactivex.Completable;
+import io.reactivex.CompletableSource;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.ext.jdbc.JDBCClient;
+import io.vertx.reactivex.ext.sql.SQLConnection;
 import org.dominokit.domino.api.server.DominoLoaderPlugin;
+import org.h2.tools.RunScript;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.sql.SQLException;
 
 @AutoService(DominoLoaderPlugin.class)
 public class TestDbClientConfiguratorPlugin extends BaseDominoLoaderPlugin {
 
     @Override
     protected void applyPlugin(CompleteHandler completeHandler) {
-        DBContext.dbClient = JDBCClient.createShared(context.getVertx(),
-                context.getConfig().getJsonObject("jdbc.db.config",
-                        new JsonObject()
-                                .put("url", "jdbc:hsqldb:mem:test?shutdown=true")
-                                .put("driver_class", "org.hsqldb.jdbcDriver")
-                                .put("max_pool_size", 30)));
-        completeHandler.onCompleted();
+        JsonObject dbConfigurations = context.getConfig().getJsonObject("jdbc.db.config",
+                new JsonObject()
+                        .put("url", "jdbc:hsqldb:mem:test?shutdown=true")
+                        .put("driver_class", "org.hsqldb.jdbcDriver")
+                        .put("max_pool_size", 30));
+        DBContext.dbClient = JDBCClient.createShared(context.getRxVertx(), dbConfigurations);
+
+        String dbScriptFile = dbConfigurations.getString("db_script_file");
+        context.getRxVertx()
+                .fileSystem()
+                .rxReadFile(dbScriptFile)
+                .flatMapCompletable(this::runScript)
+                .doFinally(completeHandler::onCompleted)
+                .subscribe();
+    }
+
+    private Completable runScript(Buffer buffer) {
+        return DBContext.dbClient.rxGetConnection()
+                .flatMapCompletable(sqlConnection -> doRunScript(buffer, sqlConnection));
+    }
+
+    private CompletableSource doRunScript(Buffer buffer, SQLConnection sqlConnection) {
+        try (Reader targetReader = new StringReader(buffer.toString())) {
+            RunScript.execute(sqlConnection.getDelegate().unwrap(), targetReader);
+            return Completable.complete();
+        } catch (SQLException | IOException e) {
+            return Completable.error(e);
+        }
     }
 
     @Override
