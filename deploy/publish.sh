c634ee48d9d643aa6a5020d151514be049970dcd
Ahmad Bawaneh <ahmad.bawaneh@progressoft.com>
2019-10-03 12:07:46 +0300

    skip tests

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -3,6 +3,6 @@
 echo $GPG_DIR
 echo "-----------------------------------"
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn clean deploy -Dmaven.test.skip=true --settings $GPG_DIR/settings.xml -Dci=true
+    mvn clean deploy -DskipTests --settings $GPG_DIR/settings.xml -Dci=true
     exit $?
 fi
