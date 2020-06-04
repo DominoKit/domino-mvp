9559aedb4827ce0087449541d8f678ed64d63298
Ahmad bawaneh <akabme@gmail.com>
2020-06-04 15:21:27 +0300

    add vertx options hook before startup

--- domino-api-server/src/main/java/org/dominokit/domino/api/server/DominoLauncher.java
+++ domino-api-server/src/main/java/org/dominokit/domino/api/server/DominoLauncher.java
@@ -2,8 +2,11 @@ package org.dominokit.domino.api.server;
 
 import io.vertx.core.Launcher;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+
+import java.util.ServiceLoader;
 
 public class DominoLauncher extends Launcher {
 
@@ -31,6 +34,15 @@ public class DominoLauncher extends Launcher {
                 PROCESS_ARGS.contains("-cluster") ?
                         routerConfigurator.configuredClusteredRouter() : routerConfigurator.configuredRouter();
 
+    }
+
+    @Override
+    public void beforeStartingVertx(VertxOptions options) {
+        ServiceLoader.load(VertxOptionsHandler.class)
+                .iterator()
+                .forEachRemaining(vertxOptionsHandler -> {
+                    vertxOptionsHandler.onBeforeVertxStart(options, configHolder.config);
+                });
     }
 
     @Override
