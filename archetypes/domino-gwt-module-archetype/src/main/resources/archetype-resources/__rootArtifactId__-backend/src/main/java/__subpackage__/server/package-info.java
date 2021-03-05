0ecec474c1d2d888e7b0dea8d458c1305439a013
Ahmad bawaneh <akabme@gmail.com>
2021-03-05 21:08:48 +0200

    update to latest snapshots versions

--- archetypes/domino-gwt-module-archetype/src/main/resources/archetype-resources/__rootArtifactId__-backend/src/main/java/__subpackage__/server/package-info.java
+++
@@ -1,7 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-@ServerModule(name="${module}")
-package ${package}.${subpackage}.server;
-
-import org.dominokit.domino.api.server.config.ServerModule;
--- archetypes/domino-gwt-module-archetype/src/main/resources/archetype-resources/__rootArtifactId__-frontend/src/main/java/__subpackage__/client/views/__module__View.java
+++
@@ -1,9 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.${subpackage}.client.views;
-
-import org.dominokit.domino.api.client.mvp.view.ContentView;
-
-public interface ${module}View extends ContentView{
-}
